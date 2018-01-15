98fc9211ffa528655bd1a84f1b7c7fef2599b2c0
NNK1122 <35451862+NNK1122@users.noreply.github.com>
2018-01-15 23:23:27 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = '5+/arDjJ9AyQRUJejcPa9ov3jSRrCLB94N9a7wXWCvHgq/LgrkCKva0D0c8nrD8KdV5/i8sqhiVPZw+25kGkvHl6aEZjP0CAnUoPZBJ7zATKkgSMDwU8lwIvfTbGwgiz85NpWO0D8bFw1U87e/1E4AdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
